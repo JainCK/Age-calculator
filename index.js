58b2934e70a3ebb1d54118350822191980892c8c
zippyzag88 <kuriakosejain456@gmail.com>
2023-10-05 22:38:49 +0530

    Added Js and events required

---
+++ index.js
@@ -0,0 +1,41 @@
+function calcAge() {
+    let birthDay = document.getElementbirthYearId("birthDay").value;
+    let birthMonth = document.getElementbirthYearId("birthMonth").value;
+    let birthYear = document.getElementbirthYearId("birthYear").value;
+    let inputElement = document.getElementbirthYearId("input-container");
+
+    if(birthDay === '' || birthMonth === '' || birthYear === '') {
+        inputElement.classList.add("error");
+        return;
+    }
+
+    let date = new Date();
+    let currentDay = date.getDate();
+    let currentMonth = 1 + date.getMonth();
+    let currentYear = date.getFullYear();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (birthDay > currentDay){
+        currentDay = currentDay + month[currentMonth-1];
+        currentMonth = currentMonth-1;
+    }
+    if(birthMonth > currentMonth){
+        currentMonth = currentMonth + 12;
+        currentYear = currentYear - 1;
+      }
+
+      let d = currentDay - birthDay;
+    let m = currentMonth - birthMonth;
+    let y = (currentYear - birthYear);
+
+    let yearsResultDiv = document.getElementById("yearsResult");
+    let monthsResultDiv = document.getElementById("monthsResult");
+    let daysResultDiv = document.getElementById("daysResult");
+
+    yearsResultDiv.innerHTML = y;
+    monthsResultDiv.innerHTML = m;
+    daysResultDiv.innerHTML = d;
+
+    inputElement.classList.remove("error");
+
+}
